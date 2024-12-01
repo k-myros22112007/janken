792245b782de4ad1e23146388b85a3465ac31b09
k9299252 <k9299252@gmail.com>
2024-12-01 11:14:19 +0900

    移動

---
+++ Assets/Scenes/start.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using static global;
+
+public class start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // シーン内のすべてのボタンを取得
+        Button[] buttons = FindObjectsOfType<Button>();
+
+        foreach (Button button in buttons)
+        {
+            // ボタンのクリックイベントにメソッドを追加
+            button.onClick.AddListener(() => OnButtonClick(button));
+        }
+    }
+
+
+    void OnButtonClick(Button button)
+    {
+        SceneManager.LoadScene("matching");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
