47733d70d738b26d508b03df199b57d7663d29a1
k9299252 <k9299252@gmail.com>
2024-12-01 14:44:52 +0900

    あいうえお

--- Assets/Scenes/card_click.cs
+++ Assets/Scenes/card_click.cs
@@ -11,7 +11,134 @@ public class card_click : MonoBehaviourPun
     int hand = 0;
 
     bool p1 = false;
-    bool p2 = false;
+    bool p2 =false;
+
+    GameObject ob;
+
+    Vector3 po= new Vector3(100, 100, 0);
+
+    void Update()
+    {
+        if (p1 == true && p2 == true)
+        {
+            //一回毎の結果
+            mini_result();
+
+            p1 = false;
+            p2 = false;
+        }
+    }
+
+
+    IEnumerator WaitForSecondsCoroutine()
+    {
+        // 5秒待機
+        yield return new WaitForSeconds(5f);
+    }
+
+    public void mini_result()
+    {
+
+        //card裏を移動(消す)
+        Vector3 po = new Vector3(100, 100, 0);
+
+        switch (global_1.round_count)
+        {
+            case 1:
+                // オブジェクト名で指定して検索し、移動
+
+                GameObject targetObject5 = GameObject.Find("ura_1_5");
+                targetObject5.transform.position = po;
+                break;
+
+            case 2:
+                // オブジェクト名で指定して検索し、移動
+
+                GameObject targetObject4 = GameObject.Find("ura_1_4");
+                targetObject4.transform.position = po;
+                break;
+
+            case 3:
+                // オブジェクト名で指定して検索し、移動
+                GameObject targetObject3 = GameObject.Find("ura_1_3");
+                targetObject3.transform.position = po;
+                break;
+
+            case 4:
+                // オブジェクト名で指定して検索し、移動
+                GameObject targetObject2 = GameObject.Find("ura_1_2");
+                targetObject2.transform.position = po;
+                break;
+
+            case 5:
+                // オブジェクト名で指定して検索し、移動
+                GameObject targetObject1 = GameObject.Find("ura_1_1");
+                targetObject1.transform.position = po;
+                break;
+
+            default:
+                Debug.LogError("Invalid random number generated.");
+                break;
+        }
+
+        Vector3 po2 = new Vector3(-1.5f, 0, 0);
+
+        //card_1_4を移動(表示)
+        switch (hand)
+        {
+            case 1:
+                // オブジェクト名で指定して検索し、移動
+                GameObject targetObject_g = GameObject.Find("gu_1_4");
+                targetObject_g.transform.position = po2;
+                break;
+            
+            case 2:
+                // オブジェクト名で指定して検索し、移動
+                GameObject targetObject_t = GameObject.Find("tyoki_1_4");
+                targetObject_t.transform.position = po2;
+                break;
+
+            case 3:
+                // オブジェクト名で指定して検索し、移動
+                GameObject targetObject_p1 = GameObject.Find("pa_1_4");
+                targetObject_p1.transform.position = po2;
+                break;
+        }
+
+        //５秒間見せる
+        StartCoroutine(WaitForSecondsCoroutine());
+
+
+        //自分が選んだcardを移動(消す)
+        po2 = new Vector3(100, 100, 0);
+        GameObject targetObject_p = GameObject.Find("pa_1_4");
+
+
+        //card_1_4を移動(消す)
+
+        switch (hand)
+        {
+            case 1:
+                // オブジェクト名で指定して検索し、移動
+                GameObject targetObject_g = GameObject.Find("gu_1_4");
+                targetObject_g.transform.position = po2;
+                break;
+
+            case 2:
+                // オブジェクト名で指定して検索し、移動
+                GameObject targetObject_t = GameObject.Find("tyoki_1_4");
+                targetObject_t.transform.position = po2;
+                break;
+
+            case 3:
+                // オブジェクト名で指定して検索し、移動
+                GameObject targetObject_p1 = GameObject.Find("pa_1_4");
+                targetObject_p1.transform.position = po2;
+                break;
+        }
+
+    }
+    
 
     public class ClickListener : MonoBehaviour
     {
@@ -24,6 +151,8 @@ public class card_click : MonoBehaviourPun
             {
                 // クリックされたオブジェクトの名前を表示
                 Debug.Log($"{targetObject.name} was clicked!");
+
+                
             }
         }
 
@@ -33,6 +162,9 @@ public class card_click : MonoBehaviourPun
             targetObject = obj;
         }
     }
+    
+
+
     // 移動先の座標
     public Vector3 targetPosition = new Vector3(1.5f, 0f, 0f);
 
@@ -56,7 +188,8 @@ public class card_click : MonoBehaviourPun
         {
             transform.position = targetPosition;
             objectMoved = true; // フラグを立てて他のオブジェクトが移動しないようにする
-            Debug.Log($"{gameObject.name} moved to {targetPosition}");
+            Debug.Log($"{gameObject.name} movedhhhhhhhhhhhhhhhhhhhh to {targetPosition}");
+
             if (gameObject.CompareTag("gu_1") || gameObject.CompareTag("gu_2") || gameObject.CompareTag("gu_3") )
             {
                 hand = 1;
@@ -76,9 +209,6 @@ public class card_click : MonoBehaviourPun
             // 他のプレイヤーに手を送信するRPCを呼び出す
             photonView.RPC("ReceiveJankenHand", RpcTarget.Others, hand);
         }
-
-
-
     }
 
     // Start is called before the first frame update
@@ -136,6 +266,7 @@ public class card_click : MonoBehaviourPun
 
         transform.Translate(-1.5f, 0, 0);
 
+
         Vector3 newPosition = new Vector3(100, 100, 0);
 
         switch (global_1.round_count)
@@ -182,4 +313,6 @@ public class card_click : MonoBehaviourPun
     {
         photonView.RPC("ReceiveJankenHand", RpcTarget.Others, hand);
     }
+
+
 }
