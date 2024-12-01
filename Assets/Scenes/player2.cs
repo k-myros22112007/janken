0853a2910089b256186f9a7d4f7b9d4bb6a8a366
yakiniku2024 <myros22112014@gmail.com>
2024-12-01 12:56:05 +0900

    vsシーンに移動

---
+++ Assets/Scenes/player2.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using static global;
+
+public class player2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // シーン内のすべてのボタンを取得
+        Button[] buttons = FindObjectsOfType<Button>();
+
+        foreach (Button button in buttons)
+        {
+            // ボタンのクリックイベントにメソッドを追加
+            button.onClick.AddListener(() => OnButtonClick(button));
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnButtonClick(Button button)
+    {
+        SceneManager.LoadScene("vs_2");
+    }
+
+}
